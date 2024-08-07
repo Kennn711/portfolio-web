a67a7d7ffd18682c17bb3fd2c5345067b2346402
Kennn <marcell040507@gmail.com>
2024-08-07 11:00:17 +0700

    Create success.php

---
+++ success.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio | Submit Successful</title>
+    <link rel="stylesheet" href="bootstrap/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="success.css">
+</head>
+<body>
+    <div class="container mt-5">
+        <div class="alert alert-success" role="alert">
+            <h4 class="alert-heading">Success!</h4>
+            <p>Your message has been successfully sent.</p>
+            <hr>
+            <p class="mb-0">Thank you for contacting me <i class="bi bi-emoji-smile-fill"></i> <a href="index.html" class="fw-bold">Go back to the homepage</a>.</p>
+        </div>
+    </div>
+</body>
+</html>
